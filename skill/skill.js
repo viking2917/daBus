71113b96a7be4422e576f8cab719f1abcc3ccf09
viking2917 <viking2917@gmail.com>
2018-07-16 14:03:36 -1000

    formatting

--- skill/skill.js
+++ skill/skill.js
@@ -1,46 +1,30 @@
 {
     "interactionModel": {
-        "languageModel": {
-            "invocationName": "da bus",
-            "intents": [
-                {
-                    "name": "AMAZON.FallbackIntent",
-                    "samples": []
-                },
-                {
-                    "name": "AMAZON.CancelIntent",
-                    "samples": []
-                },
-                {
-                    "name": "AMAZON.HelpIntent",
-                    "samples": []
-                },
-                {
-                    "name": "AMAZON.StopIntent",
-                    "samples": []
-                },
-                {
-                    "name": "nextBus",
-                    "slots": [
-                        {
-                            "name": "Stop",
-                            "type": "AMAZON.NUMBER"
-                        }
-                    ],
-                    "samples": [
-                        "Stop number {Stop}",
-                        "{Stop}",
-                        "Number {Stop}", 
-			"I live near stop {Stop}",
-                        "what about {Stop}",
-			"when is the next bus for stop {Stop}",
-			"when is the next bus at stop {Stop}",
-			"when is the next bus at {Stop}",
-			"when's the next bus at {Stop}"
-                    ]
-                }
-            ],
-            "types": []
-        }
+	"languageModel": {
+	    "invocationName": "da bus",
+	    "intents": [
+		{ "name": "AMAZON.FallbackIntent", "samples": [] },
+		{ "name": "AMAZON.CancelIntent", "samples": [] },
+		{ "name": "AMAZON.HelpIntent", "samples": [] },
+		{ "name": "AMAZON.StopIntent", "samples": [] },
+		{ "name": "nextBus",
+		  "slots": [ { "name": "Stop",
+			       "type": "AMAZON.NUMBER"
+			     }
+			   ],
+		  "samples": [ "Stop number {Stop}",
+			       "{Stop}",
+			       "Number {Stop}", 
+			       "I live near stop {Stop}",
+			       "what about {Stop}",
+			       "when is the next bus for stop {Stop}",
+			       "when is the next bus at stop {Stop}",
+			       "when is the next bus at {Stop}",
+			       "when's the next bus at {Stop}"
+			     ]
+		}
+	    ],
+	    "types": []
+	}
     }
 }
